6ab3a7d60899fee65a161aa0ebdb57180dd00deb
kitoha <kitoha@daum.net>
2025-08-07 23:32:44 +0900

    [main] oss-elasticsearch-lab 추가.

---
+++ oss-elasticsearch-lab/src/main/java/com/osslab/elasticsearch/OssElasticsearchLabApplication.java
@@ -0,0 +1,13 @@
+package com.osslab.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssElasticsearchLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssElasticsearchLabApplication.class, args);
+    }
+
+}
