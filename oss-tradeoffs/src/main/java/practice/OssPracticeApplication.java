1e8c2d32c9772f368ea9698e50d3688f3cec4b69
kitoha <kitoha@daum.net>
2025-09-18 23:07:18 +0900

    Add Prometheus and Grafana configuration with Spring Boot integration

---
+++ oss-tradeoffs/src/main/java/practice/OssPracticeApplication.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssPracticeApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(OssPracticeApplication.class, args);
+  }
+
+}
